d94496bb96aad11c96c5c34627bf7d62bab6d6d4
Keerthan-Reddy <90082358+Keerthan-Reddy@users.noreply.github.com>
2023-12-22 19:52:56 +0530

    template commit

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -3,9 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+rooms = [
+    {'id':1,'name':'people learning python'},
+    {'id':2,'name':'learning react'},
+    {'id':3,'name':'introduction to flask'},
+]
 
 def home(request):
-    return HttpResponse('home page')
+    context ={'rooms': rooms} 
+    return render(request,'base/home.html',context)
 
-def room(request):
-    return HttpResponse('this is room page')
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id']==int(pk):
+            room=i
+    context={'room':room}
+    return render(request,'base/room.html',context)
