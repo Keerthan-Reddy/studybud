482a2af56c4e20ec544f7576301aa6803527c55e
Keerthan-Reddy <90082358+Keerthan-Reddy@users.noreply.github.com>
2023-12-25 11:29:48 +0530

    CURD operation django push

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -10,12 +10,15 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    host=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     topic=models.ForeignKey(Topic,on_delete=models.SET_NULL,null=True)
     name=models.CharField( max_length=100)
     description=models.CharField(max_length=100,null=True,blank=True)
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering=['-updated','-created']
     
     def __str__(self):
         return self.name
